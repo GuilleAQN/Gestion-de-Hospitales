e9641fc39e5378456e9a91f1f05f361b51e78af1
GuilleAQN <omarguillermo1@gmail.com>
2024-06-09 11:46:25 -0400

    feat: Inclusión de estructura de UnitTest y IntegrationTest

---
+++ GestionDeHospitales.TestIntegracion/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace GestionDeHospitales.TestIntegracion;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ GestionDeHospitales.TestUnitarios/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace GestionDeHospitales.TestUnitarios;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
